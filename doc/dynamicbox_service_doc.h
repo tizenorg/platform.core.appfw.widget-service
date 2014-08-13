6ffc87a414a0f990f31d00b9ddb291d84d075b78
Sung-jae Park <nicesj.park@samsung.com>
2014-08-13 14:42:22 +0900

    Update documentations
    Change-Id: Ia9a230c735dd42bd9476be43e79712aeddfaabc3

--- doc/dynamicbox_service_doc.h
+++ doc/dynamicbox_service_doc.h
@@ -33,72 +33,79 @@ The information of installed Dynamic Boxes are stored in the system DB.
 In some cases, application developer needs to send update request to its Dynamic Box.
 To make it possible, this module provider such kinds of API set.
 
-Those APIs are communicate with master service provider called data-provider-master. When it receives request from applications, it will find the proper service provider called data-provider-slave or application Dynamic Box provider.
-If it finds proper service provider, it will forward the requests to the service provider.
+Those APIs are communicate with master service provider called data-provider-master.
+When it receives request from applications, it will find the proper service provider called data-provider-slave or application Dynamic Box Provider.
+If master finds proper service provider, it will forward the requests to them.
 Then each Dynamic Box application can gets update request.
 
 <H2>1.1 APIs for retrieving information of Dynamic Boxes</H2>
 
-Using following APIs, you can read XML data which are stored in DB.
+To get the information for installed dynamic boxes.
 
 <H2>1.2 APIs for communicating with running Dynamic Box instances</H2>
 
+To trigger the update event or send the updated content string to the dynamic box.
+
 <H1>2. Size of Dynamic Box</H1>
+
 Dynamic Box has predefined size types.
 
 <TABLE>
-<TR>
+<TH>
 <TD>Size Type</TD><TD>Size in pixels</TD><TD>Supported profile</TD>
+</TH>
+<TR>
+<TD>DBOX_SIZE_TYPE_1x1</TD><TD> - </TD><TD>Mobile, Wearable - Mandatory size</TD>
 </TR>
 <TR>
-<TD>LB_SIZE_TYPE_1x1</TD><TD> - </TD><TD>Redwood</TD>
+<TD>DBOX_SIZE_TYPE_2x1</TD><TD> - </TD><TD>Mobile</TD>
 </TR>
 <TR>
-<TD>LB_SIZE_TYPE_2x1</TD><TD> - </TD><TD>Redwood</TD>
+<TD>DBOX_SIZE_TYPE_2x2</TD><TD> - </TD><TD>Mobile, Wearable</TD>
 </TR>
 <TR>
-<TD>LB_SIZE_TYPE_2x2</TD><TD> - </TD><TD>Redwood, Wearable</TD>
+<TD>DBOX_SIZE_TYPE_4x1</TD><TD> - </TD><TD>Mobile - Extension</TD>
 </TR>
 <TR>
-<TD>LB_SIZE_TYPE_4x1</TD><TD> - </TD><TD>Redwood</TD>
+<TD>DBOX_SIZE_TYPE_4x2</TD><TD> - </TD><TD>Mobile - Since 2.3</TD>
 </TR>
 <TR>
-<TD>LB_SIZE_TYPE_4x2</TD><TD> - </TD><TD>Redwood</TD>
+<TD>DBOX_SIZE_TYPE_4x3</TD><TD> - </TD><TD>Mobile - Extension</TD>
 </TR>
 <TR>
-<TD>LB_SIZE_TYPE_4x3</TD><TD> - </TD><TD>Redwood</TD>
+<TD>DBOX_SIZE_TYPE_4x4</TD><TD> - </TD><TD>Mobile, Kiran - Since 2.3</TD>
 </TR>
 <TR>
-<TD>LB_SIZE_TYPE_4x4</TD><TD> - </TD><TD>Redwood, Kiran</TD>
+<TD>DBOX_SIZE_TYPE_4x5</TD><TD> - </TD><TD>Mobile - Extension</TD>
 </TR>
 <TR>
-<TD>LB_SIZE_TYPE_4x5</TD><TD> - </TD><TD>Redwood</TD>
-</TR>
-<TR>
-<TD>LB_SIZE_TYPE_4x6</TD><TD> - </TD><TD>Redwood</TD>
+<TD>DBOX_SIZE_TYPE_4x6</TD><TD> - </TD><TD>Mobile - Extension</TD>
 </TR>
 </TABLE>
 
-In case of samsung only types,
-There are Easy Mode
+Dynamic Box also handles Easy Mode.
+For the easy mode, there are different size types.
+Called DBOX_SIZE_TYPE_EASY_NxM
+
 <TABLE>
-<TR>
+<TH>
 <TD>Size Type</TD><TD>Size in pixels</TD><TD>Supported profile</TD>
+</TH>
+<TR>
+<TD>DBOX_SIZE_TYPE_EASY_1x1</TD><TD> - </TD><TD>Mobile</TD>
 </TR>
 <TR>
-<TD>LB_SIZE_TYPE_EASY_1x1</TD><TD> - </TD><TD>Redwood</TD>
+<TD>DBOX_SIZE_TYPE_EASY_3x1</TD><TD> - </TD><TD>Mobile</TD>
 </TR>
 <TR>
-<TD>LB_SIZE_TYPE_EASY_3x1</TD><TD> - </TD><TD>Redwood</TD>
-</TR>
-<TR>
-<TD>LB_SIZE_TYPE_EASY_3x3</TD><TD> - </TD><TD>Redwood</TD>
+<TD>DBOX_SIZE_TYPE_EASY_3x3</TD><TD> - </TD><TD>Mobile</TD>
 </TR>
 </TABLE>
 
-But in case of easy mode, it is not officially supported.
-So you will not be able to create your Dynamic Boxes using these size types.
-if you are developing the homescreen, you should care these types of Dynamic Boxes.
+However the easy mode is not officially supported.
+So you will not be able to create your Dynamic Boxes using easy mode size types.
+If you are developing the homescreen(or application which is related with dynamic boxes),
+You should care these types too.
 
  *
  */
