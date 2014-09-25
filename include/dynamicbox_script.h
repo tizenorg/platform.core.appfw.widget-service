79efb7f7435f38cc432a5edb254ff4396d7ef85f
Sung-jae Park <nicesj.park@samsung.com>
2014-09-25 22:28:25 +0900

    Move some definitions to dynamicbox_script.
    [model] Redwood,Kiran,B3(Wearable)
    [binary_type] AP
    [customer] Docomo/Orange/ATT/Open
    [issue#] N/A
    [problem]
    [cause]
    [solution]
    [team] HomeTF
    [request]
    [horizontal_expansion]
    
    Change-Id: I1b22e39bc2718e132805836d545aea803d4d01bc

--- include/dynamicbox_script.h
+++ include/dynamicbox_script.h
@@ -33,6 +33,51 @@ extern "C" {
 
 /**
  * @internal
+ * @brief Enumeration for script event of dynamicbox.
+ * @details
+ * This enumeration values will be used for script plugin of data-provider-master.
+ * Master will send down these accessibility events to the script plugin.
+ * @since_tizen 2.3
+ */
+enum dynamicbox_script_event {
+    DBOX_SCRIPT_ACCESS_EVENT          = 0x01000000, /**< Mask value */
+    DBOX_SCRIPT_MOUSE_EVENT           = 0x02000000, /**< Mask value */
+    DBOX_SCRIPT_KEY_EVENT             = 0x04000000, /**< Mask value */
+
+    DBOX_SCRIPT_ACCESS_HIGHLIGHT      = DBOX_SCRIPT_ACCESS_EVENT | 0x00000001, /**< Highlight */
+    DBOX_SCRIPT_ACCESS_HIGHLIGHT_NEXT = DBOX_SCRIPT_ACCESS_EVENT | 0x00000002, /**< Move Highlight focus to the next object */
+    DBOX_SCRIPT_ACCESS_HIGHLIGHT_PREV = DBOX_SCRIPT_ACCESS_EVENT | 0x00000004, /**< Move Highlight focus to the prev object */
+    DBOX_SCRIPT_ACCESS_ACTIVATE       = DBOX_SCRIPT_ACCESS_EVENT | 0x00000008, /**< Activate the highlighted object */
+    DBOX_SCRIPT_ACCESS_ACTION         = DBOX_SCRIPT_ACCESS_EVENT | 0x00000010, /**< Do specified action for the highlighted object */
+    DBOX_SCRIPT_ACCESS_SCROLL         = DBOX_SCRIPT_ACCESS_EVENT | 0x00000020, /**< Scroll operation */
+    DBOX_SCRIPT_ACCESS_UNHIGHLIGHT    = DBOX_SCRIPT_ACCESS_EVENT | 0x00000040, /**< Remove highlight */
+    DBOX_SCRIPT_ACCESS_VALUE_CHANGE   = DBOX_SCRIPT_ACCESS_EVENT | 0x00000080, /* TODO: deprecate this */
+    DBOX_SCRIPT_ACCESS_MOUSE          = DBOX_SCRIPT_ACCESS_EVENT | 0x00000100, /* give mouse event to highlight object */
+    DBOX_SCRIPT_ACCESS_BACK           = DBOX_SCRIPT_ACCESS_EVENT | 0x00000200, /* go back to a previous view ex: pop naviframe item */
+    DBOX_SCRIPT_ACCESS_OVER           = DBOX_SCRIPT_ACCESS_EVENT | 0x00000400, /* mouse over an object */
+    DBOX_SCRIPT_ACCESS_READ           = DBOX_SCRIPT_ACCESS_EVENT | 0x00000800, /* highlight an object */
+    DBOX_SCRIPT_ACCESS_ENABLE         = DBOX_SCRIPT_ACCESS_EVENT | 0x00001000, /* enable highlight and read ability */
+    DBOX_SCRIPT_ACCESS_DISABLE        = DBOX_SCRIPT_ACCESS_EVENT | 0x00002000, /* disable highlight and read ability */
+    
+    DBOX_SCRIPT_MOUSE_DOWN            = DBOX_SCRIPT_MOUSE_EVENT | 0x00001000, /**< Mouse down */
+    DBOX_SCRIPT_MOUSE_MOVE            = DBOX_SCRIPT_MOUSE_EVENT | 0x00002000, /**< Mouse move */
+    DBOX_SCRIPT_MOUSE_UP              = DBOX_SCRIPT_MOUSE_EVENT | 0x00004000, /**< Mouse up */
+    DBOX_SCRIPT_MOUSE_IN              = DBOX_SCRIPT_MOUSE_EVENT | 0x00008000, /**< Mouse in */
+    DBOX_SCRIPT_MOUSE_OUT             = DBOX_SCRIPT_MOUSE_EVENT | 0x00010000, /**< Mouse out */
+    
+    DBOX_SCRIPT_MOUSE_ON_SCROLL       = DBOX_SCRIPT_MOUSE_EVENT | 0x00020000, /**< If the box in in scroller and it is scrolled */
+    DBOX_SCRIPT_MOUSE_OFF_SCROLL      = DBOX_SCRIPT_MOUSE_EVENT | 0x00040000, /**< If the box is in scroller but the scroll is stopped */
+    DBOX_SCRIPT_MOUSE_ON_HOLD         = DBOX_SCRIPT_MOUSE_EVENT | 0x00080000, /**< Even though the box gets mouse_up event, the click event will not be generated */
+    DBOX_SCRIPT_MOUSE_OFF_HOLD        = DBOX_SCRIPT_MOUSE_EVENT | 0x00100000, /**< Generate the click event if the mouse_up event occurred as normal */
+    
+    DBOX_SCRIPT_KEY_DOWN              = DBOX_SCRIPT_KEY_EVENT | 0x00020000, /**< Key pressed */
+    DBOX_SCRIPT_KEY_UP                = DBOX_SCRIPT_KEY_EVENT | 0x00040000, /**< Key released */
+    DBOX_SCRIPT_KEY_FOCUS_IN          = DBOX_SCRIPT_KEY_EVENT | 0x00080000, /**< Key focus in */
+    DBOX_SCRIPT_KEY_FOCUS_OUT         = DBOX_SCRIPT_KEY_EVENT | 0x00100000, /**< Key focus out */
+};
+
+/**
+ * @internal
  * @brief
  * Text signal & Content event uses this data structure.
  * @since_tizen 2.3
