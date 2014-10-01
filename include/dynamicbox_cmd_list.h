e481e19422fbc32fb4406c2b2b4029275bd9372b
Sung-jae Park <nicesj.park@samsung.com>
2014-10-01 21:50:27 +0900

    Add constant string definitions for command
    [model] Redwood,Kiran,B3(Wearable)
    [binary_type] AP
    [customer] Docomo/Orange/ATT/Open
    [issue#] N/A
    [problem]
    [cause]
    [solution]
    [team] HomeTF
    [request]
    [horizontal_expansion]
    
    Change-Id: I3289ba5c1e40f36e4dd889da576c49deb30f5586

--- include/dynamicbox_cmd_list.h
+++ include/dynamicbox_cmd_list.h
@@ -53,6 +53,23 @@ extern "C" {
 #define CMD_KEY_STATUS         0x00000B01
 #define CMD_CLOSE_GBAR         0x00000C01
 
+#define CMD_STR_UPDATED           "updated"
+#define CMD_STR_DBOX_UPDATED      "dbox_updated"
+#define CMD_STR_DESC_UPDATED      "desc_updated"
+#define CMD_STR_GBAR_UPDATED      "gbar_updated"
+#define CMD_STR_EXTRA_INFO        "extra_info"
+#define CMD_STR_DELETED           "deleted"
+#define CMD_STR_FAULTED           "faulted"
+#define CMD_STR_FAULT_PACKAGE     "fault_package"
+#define CMD_STR_SCROLL            "scroll"
+#define CMD_STR_DBOX_UPDATE_BEGIN "dbox_update_begin"
+#define CMD_STR_DBOX_UPDATE_END   "dbox_update_end"
+#define CMD_STR_GBAR_UPDATE_BEGIN "gbar_update_begin"
+#define CMD_STR_GBAR_UPDATE_END   "gbar_update_end"
+#define CMD_STR_ACCESS_STATUS     "access_status"
+#define CMD_STR_KEY_STATUS        "key_status"
+#define CMD_STR_CLOSE_GBAR        "close_gbar"
+
 /**
  * @brief Command list for
  *	  Provder to Master.
@@ -66,6 +83,16 @@ extern "C" {
 #define CMD_PING               0x00001301
 #define CMD_CTRL               0x00001401
 
+#define CMD_STR_CALL           "call"
+#define CMD_STR_RET            "ret"
+#define CMD_STR_ACQUIRE_BUFFER "acquire_buffer"
+#define CMD_STR_RESIZE_BUFFER  "resize_buffer"
+#define CMD_STR_RELEASE_BUFFER "release_buffer"
+#define CMD_STR_HELLO          "hello"
+#define CMD_STR_PING           "ping"
+#define CMD_STR_CTRL           "ctrl"
+
+
 /**
  * @brief Master to Viewer
  */
@@ -78,21 +105,42 @@ extern "C" {
 #define CMD_RESULT_PINUP       0x00001301   /**< To Viewer */
 #define CMD_RESULT_UPDATE_MODE 0x00001401   /**< To Viewer */
 
+#define CMD_STR_GBAR_CREATED       "gbar_created"
+#define CMD_STR_GBAR_DESTROYED     "gbar_destroyed"
+#define CMD_STR_CREATED            "created"
+#define CMD_STR_GROUP_CHANGED      "group_changed"
+#define CMD_STR_PERIOD_CHANGED     "period_changed"
+#define CMD_STR_SIZE_CHANGED       "size_changed"
+#define CMD_STR_RESULT_PINUP       "pinup"
+#define CMD_STR_RESULT_UPDATE_MODE "update_mode"
+
 /**
  * @brief Following list should be started after the common list of client_cmd_list
- *        Master to Viewer
+ *        Master to Provider
  */
-#define CMD_GBAR_SHOW          0x00003701
-#define CMD_GBAR_HIDE          0x00003801
-#define CMD_DBOX_PAUSE         0x00003901
-#define CMD_DBOX_RESUME        0x00003A01
-#define CMD_SCRIPT             0x00003B01
-#define CMD_RENEW              0x00003C01
-#define CMD_PINUP              0x00003D01
-#define CMD_UPDATE_CONTENT     0x00003E01
-#define CMD_PAUSE              0x00003F01
-#define CMD_RESUME             0x00004001
-#define CMD_DISCONNECT         0x00004101
+#define CMD_GBAR_SHOW      0x00003701
+#define CMD_GBAR_HIDE      0x00003801
+#define CMD_DBOX_PAUSE     0x00003901
+#define CMD_DBOX_RESUME    0x00003A01
+#define CMD_SCRIPT         0x00003B01
+#define CMD_RENEW          0x00003C01
+#define CMD_PINUP          0x00003D01
+#define CMD_UPDATE_CONTENT 0x00003E01
+#define CMD_PAUSE          0x00003F01
+#define CMD_RESUME         0x00004001
+#define CMD_DISCONNECT     0x00004101
+
+#define CMD_STR_GBAR_SHOW      "gbar_show"
+#define CMD_STR_GBAR_HIDE      "gbar_hide"
+#define CMD_STR_DBOX_PAUSE     "dbox_pause"
+#define CMD_STR_DBOX_RESUME    "dbox_resume"
+#define CMD_STR_SCRIPT         "script"
+#define CMD_STR_RENEW          "renew"
+#define CMD_STR_PINUP          "pinup"
+#define CMD_STR_UPDATE_CONTENT "update_content"
+#define CMD_STR_PAUSE          "pause"
+#define CMD_STR_RESUME         "resume"
+#define CMD_STR_DISCONNECT     "disconnect"
 
 /**
  * @brief liveinfo to master
@@ -104,6 +152,14 @@ extern "C" {
 #define CMD_INFO_SLAVE_CTRL    0x00000401
 #define CMD_INFO_PKG_CTRL      0x00000501
 #define CMD_INFO_MASTER_CTRL   0x00000601
+
+#define CMD_STR_INFO_HELLO       "liveinfo_hello"
+#define CMD_STR_INFO_SLAVE_LIST  "slave_list"
+#define CMD_STR_INFO_PKG_LIST    "pkg_list"
+#define CMD_STR_INFO_INST_LIST   "inst_list"
+#define CMD_STR_INFO_SLAVE_CTRL  "slave_ctrl"
+#define CMD_STR_INFO_PKG_CTRL    "pkg_ctrl"
+#define CMD_STR_INFO_MASTER_CTRL "master_ctrl"
 
 /**
  * @brief Viewer to maser
@@ -165,6 +221,62 @@ extern "C" {
 #define CMD_GBAR_KEY_FOCUS_OUT    0x00003501
 #define CMD_UPDATE_MODE           0x00003601
 
+#define CMD_STR_GBAR_MOUSE_MOVE          "gbar_mouse_move"
+#define CMD_STR_DBOX_MOUSE_MOVE          "dbox_mouse_move"
+#define CMD_STR_GBAR_MOUSE_DOWN          "gbar_mouse_down"
+#define CMD_STR_GBAR_MOUSE_UP            "gbar_mouse_up"
+#define CMD_STR_DBOX_MOUSE_DOWN          "dbox_mouse_down"
+#define CMD_STR_DBOX_MOUSE_UP            "dbox_mouse_up"
+#define CMD_STR_GBAR_MOUSE_ENTER         "gbar_mouse_enter"
+#define CMD_STR_GBAR_MOUSE_LEAVE         "gbar_mouse_leave"
+#define CMD_STR_DBOX_MOUSE_ENTER         "dbox_mouse_enter"
+#define CMD_STR_DBOX_MOUSE_LEAVE         "dbox_mouse_leave"
+#define CMD_STR_DBOX_MOUSE_ON_SCROLL     "dbox_mouse_on_scroll"
+#define CMD_STR_DBOX_MOUSE_OFF_SCROLL    "dbox_mouse_off_scroll"
+#define CMD_STR_GBAR_MOUSE_ON_SCROLL     "gbar_mouse_on_scroll"
+#define CMD_STR_GBAR_MOUSE_OFF_SCROLL    "gbar_mouse_off_scroll"
+#define CMD_STR_DBOX_MOUSE_ON_HOLD       "dbox_mouse_on_hold"
+#define CMD_STR_DBOX_MOUSE_OFF_HOLD      "dbox_mouse_off_hold"
+#define CMD_STR_GBAR_MOUSE_ON_HOLD       "gbar_mouse_on_hold"
+#define CMD_STR_GBAR_MOUSE_OFF_HOLD      "gbar_mouse_off_hold"
+#define CMD_STR_CLICKED                  "clicked"
+#define CMD_STR_TEXT_SIGNAL              "text_signal"
+#define CMD_STR_DELETE                   "delete"
+#define CMD_STR_RESIZE                   "resize"
+#define CMD_STR_NEW                      "new"
+#define CMD_STR_SET_PERIOD               "set_period"
+#define CMD_STR_CHANGE_GROUP             "change_group"
+#define CMD_STR_GBAR_MOVE                "gbar_move"
+#define CMD_STR_GBAR_ACCESS_HL           "gbar_access_hl"
+#define CMD_STR_GBAR_ACCESS_ACTIVATE     "gbar_access_activate"
+#define CMD_STR_GBAR_ACCESS_ACTION       "gbar_access_action"
+#define CMD_STR_GBAR_ACCESS_SCROLL       "gbar_access_scroll"
+#define CMD_STR_GBAR_ACCESS_VALUE_CHANGE "gbar_access_value_change"
+#define CMD_STR_GBAR_ACCESS_MOUSE        "gbar_access_mouse"
+#define CMD_STR_GBAR_ACCESS_BACK         "gbar_access_back"
+#define CMD_STR_GBAR_ACCESS_OVER         "gbar_access_over"
+#define CMD_STR_GBAR_ACCESS_READ         "gbar_access_read"
+#define CMD_STR_GBAR_ACCESS_ENABLE       "gbar_access_enable"
+#define CMD_STR_DBOX_ACCESS_HL           "dbox_access_hl"
+#define CMD_STR_DBOX_ACCESS_ACTIVATE     "dbox_access_activate"
+#define CMD_STR_DBOX_ACCESS_ACTION       "dbox_access_action"
+#define CMD_STR_DBOX_ACCESS_SCROLL       "dbox_access_scroll"
+#define CMD_STR_DBOX_ACCESS_VALUE_CHANGE "dbox_access_value_change"
+#define CMD_STR_DBOX_ACCESS_MOUSE        "dbox_access_mouse"
+#define CMD_STR_DBOX_ACCESS_BACK         "dbox_access_back"
+#define CMD_STR_DBOX_ACCESS_OVER         "dbox_access_over"
+#define CMD_STR_DBOX_ACCESS_READ         "dbox_access_read"
+#define CMD_STR_DBOX_ACCESS_ENABLE       "dbox_access_enable"
+#define CMD_STR_DBOX_KEY_DOWN            "dbox_key_down"
+#define CMD_STR_DBOX_KEY_UP              "dbox_key_up"
+#define CMD_STR_DBOX_KEY_FOCUS_IN        "dbox_key_focus_in"
+#define CMD_STR_DBOX_KEY_FOCUS_OUT       "dbox_key_focus_out"
+#define CMD_STR_GBAR_KEY_DOWN            "gbar_key_down"
+#define CMD_STR_GBAR_KEY_UP              "gbar_key_up"
+#define CMD_STR_GBAR_KEY_FOCUS_IN        "gbar_key_focus_in"
+#define CMD_STR_GBAR_KEY_FOCUS_OUT       "gbar_key_focus_out"
+#define CMD_STR_UPDATE_MODE              "update_mode"
+
 /**
  * @brief viewer to master
  */
@@ -196,12 +308,45 @@ extern "C" {
 #define CMD_CLIENT_PAUSED         0x00005001
 #define CMD_CLIENT_RESUMED        0x00005101
 
+#define CMD_STR_DBOX_MOUSE_SET      "dbox_mouse_set"
+#define CMD_STR_DBOX_MOUSE_UNSET    "dbox_mouse_unset"
+#define CMD_STR_GBAR_MOUSE_SET      "gbar_mouse_set"
+#define CMD_STR_GBAR_MOUSE_UNSET    "gbar_mouse_unset"
+#define CMD_STR_CHANGE_VISIBILITY   "change,visibility"
+#define CMD_STR_DBOX_ACQUIRE_PIXMAP "dbox_acquire_pixmap"
+#define CMD_STR_DBOX_RELEASE_PIXMAP "dbox_release_pixmap"
+#define CMD_STR_GBAR_ACQUIRE_PIXMAP "gbar_acquire_pixmap"
+#define CMD_STR_GBAR_RELEASE_PIXMAP "gbar_release_pixmap"
+#define CMD_STR_ACQUIRE             "acquire"
+#define CMD_STR_RELEASE             "release"
+#define CMD_STR_PINUP_CHANGED       "pinup_changed"
+#define CMD_STR_CREATE_GBAR         "create_gbar"
+#define CMD_STR_DESTROY_GBAR        "destroy_gbar"
+#define CMD_STR_ACTIVATE_PACKAGE    "activate_package"
+#define CMD_STR_SUBSCRIBE           "subscribe" /* pid, cluster, sub-cluster */
+#define CMD_STR_UNSUBSCRIBE         "unsubscribe" /* pid, cluster, sub-cluster */
+#define CMD_STR_DELETE_CLUSTER      "delete_cluster"
+#define CMD_STR_DELETE_CATEGORY     "delete_category"
+#define CMD_STR_REFRESH_GROUP       "refresh_group"
+#define CMD_STR_UPDATE              "update"
+#define CMD_STR_DBOX_KEY_SET        "dbox_key_set"
+#define CMD_STR_DBOX_KEY_UNSET      "dbox_key_unset"
+#define CMD_STR_GBAR_KEY_SET        "gbar_key_set"
+#define CMD_STR_GBAR_KEY_UNSET      "gbar_key_unset"
+#define CMD_STR_CLIENT_PAUSED       "client_paused"
+#define CMD_STR_CLIENT_RESUMED      "client_resumed"
+
+
 /**
  * @brief Service API to Master
  */
 #define CMD_SERVICE_UPDATE        0x00000001
 #define CMD_SERVICE_CHANGE_PERIOD 0x00000101
 #define CMD_SERVICE_INST_CNT      0x00000201
+
+#define CMD_STR_SERVICE_UPDATE        "service_update"
+#define CMD_STR_SERVICE_CHANGE_PERIOD "service_change_period"
+#define CMD_STR_SERVICE_INST_CNT      "service_inst_cnt"
 
 #ifdef __cplusplus
 }
