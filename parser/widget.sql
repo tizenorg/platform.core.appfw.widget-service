a61c12c4731aa668d9c3ba4141f9943aa8de4ef4
Sangyoon Jang <s89.jang@samsung.com>
2015-11-18 13:28:10 +0900

    Add widget-plugin-parser source codes
    note that data structures and related functions
    at 'widget_plugin_parser_internal.h' could be separated
    
    Change-Id: I169d3523ef151538fdb5f755f0bb0528e1385d33
    Signed-off-by: Sangyoon Jang <s89.jang@samsung.com>

---
+++ parser/widget.sql
@@ -0,0 +1,40 @@
+PRAGMA tizen_version = 30; /* Tizen 3.0 */
+PRAGMA db_version = 1;
+PRAGMA journal_mode = PERSIST;
+PRAGMA foreign_keys = ON;
+
+BEGIN EXCLUSIVE TRANSACTION;
+
+CREATE TABLE widget_class (
+  classid       TEXT NOT NULL,
+  update_period INTEGER DEFAULT 0,
+  setup_appid   TEXT,
+  appid         TEXT NOT NULL,
+  pkgid         TEXT NOT NULL,
+  PRIMARY KEY(classid)
+);
+
+CREATE TABLE support_size (
+  classid TEXT NOT NULL,
+  preview TEXT NOT NULL,
+  frame   INTEGER DEFAULT 0,
+  width   INTEGER NOT NULL,
+  height  INTEGER NOT NULL,
+  FOREIGN KEY(classid) REFERENCES widget_class (classid) ON DELETE CASCADE
+);
+
+CREATE TABLE label (
+  classid TEXT NOT NULL,
+  locale  TEXT DEFAULT 'No Locale',
+  label   TEXT NOT NULL,
+  FOREIGN KEY(classid) REFERENCES widget_class (classid) ON DELETE CASCADE
+);
+
+CREATE TABLE icon (
+  classid TEXT NOT NULL,
+  locale  TEXT DEFAULT 'No Locale',
+  icon    TEXT NOT NULL,
+  FOREIGN KEY(classid) REFERENCES widget_class (classid) ON DELETE CASCADE
+);
+
+COMMIT TRANSACTION;
